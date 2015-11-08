a8945809b1d9a488f32f6cab785457d9ee04ab65
Nav Appaiya <navarajh@gmail.com>
2015-11-08 18:58:56 +0100

    project setup fix, need some mods on user entity soon

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -37,4 +37,10 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function init()
+    {
+        date_default_timezone_set( 'Europe/Amsterdam' );
+        parent::init();
+    }
 }
