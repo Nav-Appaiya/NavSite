4182023f4d419e4f15e8ac96d8b0e8e5e5302442
Nav Appaiya <navarajh@gmail.com>
2016-01-09 22:14:17 +0100

    First page of v2 commit

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -10,11 +10,28 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Core\NavController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class TestController
+ * @package AppBundle\Controller
+ */
 class TestController extends NavController
 {
+
+	/**
+     * @Route("/", name="test_index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
+//        echo '<pre>';
+//        $client = new \Github\Client();
+//        $repositories = $client->api('user')->repositories('Nav-Appaiya');
+//        $response = $client->api('repo')->commits()->all('Nav-Appaiya', 'NavSite', array('sha' => 'master'));
+    return $this->render('@App/test.html.twig');
+        print_r($response);
+        die('died at testing');
         $feedburner = $this->get('toolbox.feedburner');
         $techzine = $feedburner->load('http://feeds.feedburner.com/Tutorialzine');
         $tweakers = $feedburner->load('http://feeds.feedburner.com/tweakers/nieuws');
@@ -26,7 +43,12 @@ class TestController extends NavController
         ]);
     }
 
-    public function loadNewsAction()
+
+    /**
+     * @Route("/bootstrap", name="test_bootstrap")
+     */
+    public function bootstrapAction()
     {
+        return $this->render('@App/V2/base.html.twig');
     }
 }
